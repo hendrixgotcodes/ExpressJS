641d2b3b68483c15d4c9bad3a9504213129abc00
hendrixgotcodes <asare11samuel@gmail.com>
2020-09-18 00:39:59 +0000

    Templating with pug

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
+app.set("view engine", "pug");
+app.set("views", './views');
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 //Error Page
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "html", "error.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "html", "error.html"));
 });
-
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,23 @@
 const path = require('path');
 const express = require('express');
-const router =  express.Router();
+const router = express.Router();
 const products = require('./admin');
 
 router.get("/shop.html", (req, res, next) => {
-    console.log(products);
-    res.sendFile(path.join(__dirname,  '../','views', 'html', 'shop.html'));
+  // const prods = 
+  console.log(products);
+  // console.log(
+  //   `title: ${products.products.title}
+  //       id: ${products.products[0].id}
+  //     `
+  // )
+  // res.sendFile(path.join(__dirname,  '../','views', 'html', 'shop.html'));
+  res.render('store', {
+    prod: products.products
   });
+});
 
-router.get('/',(req, res)=>{
+router.get('/', (req, res) => {
   res.status(302).redirect('/shop.html');
 })
 
